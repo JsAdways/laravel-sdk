5f6e9735912d484f844a320727526419b88a2524
chenchien-ming <alvin@js-adways.com.tw>
2025-04-01 09:56:35 +0800

    fix: str::snake修正

--- src/Console/Commands/Traits/table_method.php
+++ src/Console/Commands/Traits/table_method.php
@@ -4,12 +4,13 @@ namespace Jsadways\LaravelSDK\Console\Commands\Traits;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 trait table_method
 {
     protected function _get_table_columns(string $table_name): bool|array
     {
-        $table_name = strtolower($table_name);
+        $table_name = Str::snake($table_name);
         if(Schema::hasTable($table_name)){
             return json_decode(json_encode(DB::select("SHOW COLUMNS FROM {$table_name}")),true);
         }
