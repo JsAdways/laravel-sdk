b9459cf73e263b1543c00ba673dcc3a39e0bcb48
chenchien-ming <alvin@js-adways.com.tw>
2025-05-28 10:06:50 +0800

    fix: generate api update

--- src/Console/Commands/GenerateApiDocs.php
+++ src/Console/Commands/GenerateApiDocs.php
@@ -19,7 +19,7 @@ class GenerateApiDocs extends Command
     public function __construct(
         private ServerRequest $ServerRequest,
         private ReadListRequest $ReadListRequest
-    ){
+    ) {
         parent::__construct();
     }
 
@@ -46,27 +46,26 @@ class GenerateApiDocs extends Command
         try {
             $this->info('------ Generate Api Docs start -------');
             $this->newLine(2);
-            $this->generate_api_docs();
+            $this->_generate_api_docs();
             $this->newLine(2);
             $this->info('------ Generate Api Docs finish -------');
             $this->newLine(2);
             $this->info('Api Docs exported successfully');
-            $this->info('Api docs url: '.Config::get('app.url').'/docs-api');
+            $this->info('Api docs url: ' . Config::get('app.url') . '/docs-api');
             $this->newLine(2);
             $this->info('------ msg start -------');
             $this->newLine(1);
-            $this->print_msg_list();
+            $this->_print_msg_list();
             $this->newLine(1);
             $this->info('------ msg finish -------');
         } catch (Throwable $e) {
             $this->error($e->getMessage());
         }
-
     }
 
-    protected function print_msg_list(): bool{
-        foreach ($this->msg_list as $msg)
-        {
+    protected function _print_msg_list(): bool
+    {
+        foreach ($this->msg_list as $msg) {
             $this->warn($msg);
         }
 
@@ -78,9 +77,9 @@ class GenerateApiDocs extends Command
      *
      * @return boolean
      */
-    protected function generate_api_docs(): bool
+    protected function _generate_api_docs(): bool
     {
-        $route_list = $this->get_routes();
+        $route_list = $this->_get_routes();
 
         // 進度條
         $bar = $this->output->createProgressBar(count($route_list));
@@ -88,7 +87,7 @@ class GenerateApiDocs extends Command
 
         $paths_content = [];
         foreach ($route_list as $route) {
-            $authenticate = $this->has_authenticate(middleware: $route->middleware());
+            $authenticate = $this->_has_authenticate(middleware: $route->middleware());
 
             $action_name = $route->getActionName();
             if (preg_match('/([^\\\\]+?)Controller@/', $action_name, $controller_name)) {
@@ -97,36 +96,45 @@ class GenerateApiDocs extends Command
                 continue;
             }
 
-            [$controller, $controller_method] = explode('@', $action_name);
+            $controller_method = explode('@', $action_name)[1];
 
-            $http_method = mb_strtolower($route->methods()[0]);
 
-            $validate_schemas = $this->get_validate_schemas(model_name: $model_name, controller_method: $controller_method);
+
+            $validate_schemas = $this->_get_validate_schemas(model_name: $model_name, controller_method: $controller_method);
             if ($validate_schemas) {
-                $param_list = $this->transform_validate_schemas(validate_schemas: $validate_schemas);
+                $param_list = $this->_transform_validate_schemas(validate_schemas: $validate_schemas);
             } else {
                 continue;
             }
+            $method_list = $route->methods();
+            foreach ($method_list as $http_method) {
+                $http_method = mb_strtolower($http_method);
 
-            $content = match ($http_method) {
-                'get' => $this->prepare_http_get_content(model_name: $model_name),
-                'post' => $this->prepare_http_post_content(model_name: $model_name, param_list: $param_list),
-                'patch' => $this->prepare_http_patch_content(model_name: $model_name, param_list: $param_list),
-                'delete' => $this->prepare_http_delete_content(param_list: $param_list),
-                default => [],
-            };
+                $content = match ($http_method) {
+                    'get' => $this->_prepare_http_get_content(model_name: $model_name),
+                    'post' => $this->_prepare_http_post_content(model_name: $model_name, param_list: $param_list),
+                    'patch' => $this->_prepare_http_patch_content(model_name: $model_name, param_list: $param_list),
+                    'put' => $this->_prepare_http_patch_content(model_name: $model_name, param_list: $param_list),
+                    'delete' => $this->_prepare_http_delete_content(param_list: $param_list),
+                    default => [],
+                };
 
-            if (!array_key_exists($route->uri(), $paths_content)) {
-                $paths_content[$route->uri()] = [];
+                if (empty($content)) {
+                    continue;
+                }
+
+                if (!array_key_exists($route->uri(), $paths_content)) {
+                    $paths_content[$route->uri()] = [];
+                }
+
+                $paths_content[$route->uri()][$http_method] = [
+                    'tags' => [$model_name],
+                    'summary' => '',
+                    'description' => '',
+                    ...$content,
+                    ...$authenticate,
+                ];
             }
-
-            $paths_content[$route->uri()][$http_method] = [
-                'tags' => [$model_name],
-                'summary' => '',
-                'description' => '',
-                ...$content,
-                ...$authenticate,
-            ];
 
             // 執行進度條
             $bar->advance();
@@ -138,14 +146,14 @@ class GenerateApiDocs extends Command
         $docs = [
             'openapi' => '3.0.3',
             'info' => [
-                'title' => Config::get('app.name').' - OpenAPI 3.0',
+                'title' => Config::get('app.name') . ' - OpenAPI 3.0',
                 'description' => '',
                 'version' => '3.0.0'
             ],
             'paths' => $paths_content,
         ];
 
-        return $this->store_api_json($docs);
+        return $this->_store_api_json($docs);
     }
 
     /**
@@ -154,7 +162,7 @@ class GenerateApiDocs extends Command
      * @param array $middleware
      * @return array
      */
-    protected function has_authenticate(array $middleware): array
+    protected function _has_authenticate(array $middleware): array
     {
         $authenticate = [];
         if (in_array('js-authenticate-middleware-alias', $middleware)) {
@@ -176,7 +184,7 @@ class GenerateApiDocs extends Command
      *
      * @return Collection
      */
-    protected function get_routes(): Collection
+    protected function _get_routes(): Collection
     {
         return collect(Route::getRoutes())->filter(function ($route) {
             $middlewares = $route->middleware();
@@ -192,7 +200,7 @@ class GenerateApiDocs extends Command
      * @param string $model_name
      * @return array|boolean
      */
-    protected function get_validate_schemas(string $model_name, string $controller_method): array|bool
+    protected function _get_validate_schemas(string $model_name, string $controller_method): array|bool
     {
         $schemas = null;
         try {
@@ -216,51 +224,86 @@ class GenerateApiDocs extends Command
      * @param array $schemas
      * @return array
      */
-    protected function transform_validate_schemas(array $validate_schemas): array
+    protected function _transform_validate_schemas(array $validate_schemas): array
     {
         $param_list = [];
 
         foreach ($validate_schemas as $param_name => $rule) {
-            // create_dimension_item_list.*.name
-            if (preg_match("/_list/", $param_name) ) {
-                $param_name_list = preg_split("/\.\*\./", $param_name);
-                if (count($param_name_list) > 1) {
-                    $param_list = array_merge_recursive($param_list, $this->prepare_param_data(array_reverse($param_name_list), $rule));
-                }
-                // "treaties.default.*.content"
-            } else if (preg_match('/\.\*\./', $param_name)) {
-                $param_name_list = preg_split("/\.\*\./", $param_name);
-
-                $param_date_list = [];
-                foreach ($param_name_list as $param) {
-                    $param_date_list = array_merge($param_date_list, preg_split("/\./", $param));
-                }
-
-                if (count($param_name_list) > 1) {
-                    $param_list = array_merge_recursive($param_list, $this->prepare_param_data(array_reverse($param_date_list), $rule));
-                }
-                // "treaties.custom.*" => "string"
-            } else if (preg_match('/\.\*$/', $param_name)) {
-                $param_name_list = preg_split("/\.\*/", $param_name);
-
-                $param_date_list = [];
-                foreach ($param_name_list as $param) {
-                    if($param === '') {
-                        $rule .= '-array';
-                        continue;
-                    }
-                    $param_date_list = array_merge($param_date_list, preg_split("/\./", $param));
-                }
-
-                if (count($param_name_list) > 1) {
-                    $param_list = array_merge_recursive($param_list, $this->prepare_param_data(array_reverse($param_date_list), $rule));
-                }
-            } else {
-                $param_list = array_merge_recursive($param_list, [$param_name => $rule]);
-            }
+            $param_list = array_merge_recursive(
+                $param_list,
+                $this->_parse_validate_schemas(param_name: $param_name, rule: $rule)
+            );
         }
 
         return $param_list;
+    }
+
+    /**
+     * 解析驗文本
+     *
+     * @param string $param_name
+     * @param string|array $rule
+     * @return array
+     */
+    protected function _parse_validate_schemas(string $param_name, string|array $rule): array
+    {
+        $param_name_list = match (true) {
+            preg_match('/\.\*$/', $param_name) === 1 => $this->_parse_schemas_array_type(param_name: $param_name),
+            preg_match('/\.\*\./', $param_name) === 1 => $this->_parse_schemas_object_type(param_name: $param_name),
+            preg_match("/_list/", $param_name) === 1 => preg_split("/\.\*\./", $param_name),
+            default => [],
+        };
+
+        $expanded_rule = $rule;
+        if (preg_match('/\.\*$/', $param_name) && is_string($rule)) {
+            $expanded_rule = $rule . '-array';
+        }
+
+        $result = [$param_name => $expanded_rule];
+        if (!empty($param_name_list) && is_string($expanded_rule)) {
+            $result = $this->_prepare_param_data(array_reverse($param_name_list), $expanded_rule);
+        }
+
+        return $result;
+    }
+
+    /**
+     * 解析 array 類型的參數名稱 ex "treaties.custom.*" => "string"
+     *
+     * @param string $param_name
+     * @return array
+     */
+    protected function _parse_schemas_array_type(string $param_name): array
+    {
+        $param_name_list = preg_split("/\.\*/", $param_name);
+
+        $param_date_list = [];
+        foreach ($param_name_list as $param) {
+            if ($param === '') {
+                break;
+            }
+            $param_date_list = array_merge($param_date_list, preg_split("/\./", $param));
+        }
+
+        return $param_date_list;
+    }
+
+    /**
+     * 解析 object 類型的參數名稱 ex "treaties.default.*.content"
+     *
+     * @param string $param_name
+     * @return array
+     */
+    protected function _parse_schemas_object_type(string $param_name): array
+    {
+        $param_name_list = preg_split("/\.\*\./", $param_name);
+
+        $param_date_list = [];
+        foreach ($param_name_list as $param) {
+            $param_date_list = array_merge($param_date_list, preg_split("/\./", $param));
+        }
+
+        return $param_date_list;
     }
 
     /**
@@ -270,7 +313,7 @@ class GenerateApiDocs extends Command
      * @param string $rule
      * @return array
      */
-    protected function prepare_param_data(array $param_list, string $rule): array
+    protected function _prepare_param_data(array $param_list, string $rule): array
     {
         $param = array_pop($param_list);
         $result = [];
@@ -278,7 +321,7 @@ class GenerateApiDocs extends Command
             $result[$param] = $rule;
             return $result;
         } else {
-            $result[$param] = $this->prepare_param_data(param_list: $param_list, rule: $rule);
+            $result[$param] = $this->_prepare_param_data(param_list: $param_list, rule: $rule);
         }
         return $result;
     }
@@ -289,11 +332,11 @@ class GenerateApiDocs extends Command
      * @param string $model_name
      * @return array
      */
-    protected function prepare_http_get_content(string $model_name): array
+    protected function _prepare_http_get_content(string $model_name): array
     {
-        $field_data = $this->get_model_field($model_name);
+        $field_data = $this->_get_model_field($model_name);
 
-        $responses = $this->get_responses(content: [
+        $responses = $this->_get_responses(content: [
             'list' => $field_data
         ]);
 
@@ -370,11 +413,11 @@ class GenerateApiDocs extends Command
      * @param array $param_data
      * @return array
      */
-    protected function prepare_http_post_content(string $model_name, array $param_list): array
+    protected function _prepare_http_post_content(string $model_name, array $param_list): array
     {
-        $content = $this->generate_request_body(param_list: $param_list);
-        $field_data = $this->get_model_field($model_name);
-        $responses = $this->get_responses(content:  $field_data);
+        $content = $this->_generate_request_body(param_list: $param_list);
+        $field_data = $this->_get_model_field($model_name);
+        $responses = $this->_get_responses(content: $field_data);
 
         return [
             ...$content,
@@ -389,11 +432,11 @@ class GenerateApiDocs extends Command
      * @param array $param_data
      * @return array
      */
-    protected function prepare_http_patch_content(string $model_name, array $param_list): array
+    protected function _prepare_http_patch_content(string $model_name, array $param_list): array
     {
-        $content = $this->generate_request_body(param_list: $param_list);
-        $field_data = $this->get_model_field($model_name);
-        $responses = $this->get_responses(content:  $field_data);
+        $content = $this->_generate_request_body(param_list: $param_list);
+        $field_data = $this->_get_model_field($model_name);
+        $responses = $this->_get_responses(content: $field_data);
 
         return [
             ...$content,
@@ -407,10 +450,10 @@ class GenerateApiDocs extends Command
      * @param array $param_data
      * @return array
      */
-    protected function prepare_http_delete_content(array $param_list): array
+    protected function _prepare_http_delete_content(array $param_list): array
     {
-        $content = $this->generate_request_body(param_list: $param_list);
-        $responses = $this->get_responses(content: ['message' => '刪除成功。']);
+        $content = $this->_generate_request_body(param_list: $param_list);
+        $responses = $this->_get_responses(content: ['message' => '刪除成功。']);
 
         return [
             ...$content,
@@ -424,7 +467,7 @@ class GenerateApiDocs extends Command
      * @param string $model_name
      * @return array
      */
-    protected function get_model_field(string $model_name): array
+    protected function _get_model_field(string $model_name): array
     {
         $factory_data = [];
         try {
@@ -434,7 +477,7 @@ class GenerateApiDocs extends Command
             }
             $factory_data = $model::factory()->make()->only($model->getFillable());
         } catch (Throwable $e) {
-            Log::info('Generate Api Docs - '.$e->getMessage());
+            Log::info('Generate Api Docs - ' . $e->getMessage());
         }
 
         return $factory_data;
@@ -445,10 +488,10 @@ class GenerateApiDocs extends Command
      * @param array $param_data
      * @return array
      */
-    protected function generate_request_body(array $param_list): array
+    protected function _generate_request_body(array $param_list): array
     {
-        $required_field = $this->extract_required_field($param_list);
-        $properties = $this->extract_properties($param_list);
+        $required_field = $this->_extract_required_field($param_list);
+        $properties = $this->_extract_properties($param_list);
 
         return [
             'requestBody' => [
@@ -471,7 +514,7 @@ class GenerateApiDocs extends Command
      * @param array $schemas_list
      * @return array
      */
-    public function extract_required_field(array $schemas_list): array
+    protected function _extract_required_field(array $schemas_list): array
     {
         $properties = [];
         foreach ($schemas_list as $param => $content) {
@@ -491,7 +534,7 @@ class GenerateApiDocs extends Command
      * @param array $schemas_list
      * @return array
      */
-    public function extract_properties(array $schemas_list): array
+    protected function _extract_properties(array $schemas_list): array
     {
         $properties = [];
         foreach ($schemas_list as $param => $content) {
@@ -533,7 +576,7 @@ class GenerateApiDocs extends Command
                         }
                     }
                     if (!$is_enums) {
-                        $items = $this->extract_properties($content);
+                        $items = $this->_extract_properties($content);
                         $properties[$param] = [
                             'type' => 'array',
                             'items' => [
@@ -552,7 +595,7 @@ class GenerateApiDocs extends Command
      *
      * @return array
      */
-    protected function get_responses(array $content): array
+    protected function _get_responses(array $content): array
     {
         return [
             'responses' => [
@@ -613,7 +656,7 @@ class GenerateApiDocs extends Command
      * @param array $docs
      * @return boolean
      */
-    protected function store_api_json(array $docs): bool
+    protected function _store_api_json(array $docs): bool
     {
         $json = json_encode($docs, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
         return File::put(public_path('api-docs.json'), $json);
